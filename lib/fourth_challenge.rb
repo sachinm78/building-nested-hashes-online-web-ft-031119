d361a350ae00eaedec71c8c4ee26b5dfe89056ad
Sachin Mahajan <sachinm78@gmail.com>
2019-03-13 16:04:28 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,4 +1,5 @@
-epic_tragedy = {
+def fourth_challenge
+  epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
